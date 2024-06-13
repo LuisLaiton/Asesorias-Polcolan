d1974faec892baaa6cf4877c1472bd7aa4bae87f
LuisLaiton <felipelaiton0@gmail.com>
2024-06-12 19:07:23 -0500

    horarios de inicio y finalizacion

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,38 @@
-console.log("Hola mundo")
+let datos = [];
+
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Ocurrió un error en la respuesta de la red');
+        }
+        const data = await response.json();
+        return data.Teachers;
+    } catch (error) {
+        console.error('Error al leer el archivo JSON:', error);
+    }
+}
+
+async function initializeData() {
+    const url = "https://raw.githubusercontent.com/LuisLaiton/Asesorias-Polcolan/Luis-Felipe-Laiton-Cortes/data/tutorships.json";
+    datos = await fetchData(url);
+
+    if (datos) {
+        console.log(datos);
+
+        showTutorials(datos[0].Tutorships.Planned);
+    }
+}
+
+function showTutorials(hoursTeacher) {
+    const $TABLA = document.getElementById("horarios_agendados");
+
+    hoursTeacher.forEach((element, index) => {
+        console.log(element.start_time, "-", element.ending_time)
+        //console.log(`Docente ${index + 1}: ${element.Name}`);
+    });
+
+   //infoTeacher.Tutorships
+}
+
+initializeData();
